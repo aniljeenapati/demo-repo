000f50c04dd802a0f6bf77895ed88c7fb9052eaa
aniljeenapati <jeenapati.anil@gmail.com>
2025-06-02 15:34:45 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -13,15 +13,3 @@ variable "instance_type" {
 variable "key_name" {
   description = "Name of the SSH key pair"
 }
-
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
