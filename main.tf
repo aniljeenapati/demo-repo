e97fabd390b4f40d2669911b0404c4abb935c88e
aniljeenapati <jeenapati.anil@gmail.com>
2025-06-02 13:19:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,67 +1,32 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
+resource "aws_instance" "flask_vm" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
 
-resource "aws_security_group" "allow_http_ssh" {
-  name        = "allow_http_ssh"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "flask_app" {
-  ami                    = "ami-0c55b159cbfafe1f0"  # Ubuntu 20.04 in us-east-1
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer.key_name
-  security_groups        = [aws_security_group.allow_http_ssh.name]
-  associate_public_ip_address = true
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update",
-      "sudo apt install -y python3-pip git",
-      "git clone https://github.com/aniljeenapati/demo-repo.git",
-      "cd demo-repo",
-      "pip3 install flask",
-      "nohup python3 app.py &"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
-    }
-  }
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y python3 git
+              pip3 install flask
+              cd /home/ec2-user
+              git clone https://github.com/aniljeenapati/demo-repo.git
+              cd demo-repo
+              python3 app.py &
+              EOF
 
   tags = {
-    Name = "FlaskAppInstance"
+    Name = "flask-demo"
   }
+
+  associate_public_ip_address = true
 }
 
-output "instance_public_ip" {
-  value = aws_instance.flask_app.public_ip
+output "public_ip" {
+  value = aws_instance.flask_vm.public_ip
 }
