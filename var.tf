78fdbd23a0a4c60a07a16739565fa5e172ed4b0f
aniljeenapati <jeenapati.anil@gmail.com>
2025-06-02 13:19:37 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,3 +1,15 @@
-variable "aws_region" {
+variable "region" {
   default = "us-east-1"
+}
+
+variable "ami" {
+  default = "ami-0c94855ba95c71c99"  # Amazon Linux 2 AMI (for us-east-1)
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
 }
